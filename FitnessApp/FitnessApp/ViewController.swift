dbfedc1d40532d8ed5f020eca1b15745f195f7f7
MAHANNAVIDU734 <mahan.navidu@gmail.com>
2023-04-27 19:40:28 +0530

    init

---
+++ FitnessApp/FitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by mrmac on 2023/4/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
