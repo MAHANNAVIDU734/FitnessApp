127736a466ae35262c0d3584cb572c6c8cd8e99f
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-04-30 21:51:12 +0530

    Create Enum.swift

---
+++ FitnessApp/FitnessApp/Helpers/Enum.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+
+enum FirestoreCollections:String{
+    case users = "users"
+}
+
+enum DefaultPlaceHolderLinks:String{
+    case user_avatar = "https://firebasestorage.googleapis.com/v0/b/fitnessapp-1cef7.appspot.com/o/default%2Fdefault_user_avatart.png?alt=media&token=4794d83b-53df-4937-ae1d-4d2382110fa0"
+}
