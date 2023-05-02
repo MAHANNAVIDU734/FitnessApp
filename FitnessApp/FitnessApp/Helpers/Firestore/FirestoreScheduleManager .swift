02e37fa035df7327806808a55d99cfa7e88bf0a7
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-02 16:19:04 +0530

    Create FirestoreScheduleManager .swift

---
+++ FitnessApp/FitnessApp/Helpers/Firestore/FirestoreScheduleManager .swift
@@ -0,0 +1,34 @@
+import FirebaseFirestore
+
+class FirestoreScheduleManager  {
+    static let shared: FirestoreScheduleManager  = {
+        let _shared = FirestoreScheduleManager ()
+        return _shared
+    }()
+    
+    func createNewScheduleOnFirestoreDb(firestoreSchedule:FirestoreSchedule,completionWithPayload:CompletionHandlerWithData?){
+        let userId = Constants.currentLoggedInFireStoreUser?.id
+        
+        if let _userId = userId{ 
+            let schedulesDocumentRef = Firestore.firestore().collection(FirestoreCollections.users.rawValue).document(_userId)
+            
+            guard let firestoreScheduleAsDictionary = firestoreSchedule.getDictionary() else {
+                completionWithPayload?(false,"Failed to Encode New User Object ",nil)
+                return
+            }
+            schedulesDocumentRef.setData(firestoreScheduleAsDictionary) { err in
+                if let err = err {
+                    completionWithPayload?(false,err.localizedDescription,nil)
+                    print("Error writing document: \(err)")
+                } else {
+                    completionWithPayload?(true,nil,nil)
+                    print("Document successfully written!")
+                }
+            }
+        }else{
+            completionWithPayload?(false,nil,nil)
+        }
+        
+    }
+    
+}
