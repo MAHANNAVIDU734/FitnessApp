b53928e5e678c6931e866c05c8376e6806165fa6
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-03 22:29:22 +0530

    Update FirestoreScheduleManager .swift

--- FitnessApp/FitnessApp/Helpers/Firestore/FirestoreScheduleManager .swift
+++ FitnessApp/FitnessApp/Helpers/Firestore/FirestoreScheduleManager .swift
@@ -31,4 +31,29 @@ class FirestoreScheduleManager  {
         
     }
     
+    func updaetCustomScheduleOnFirestoreDb(firestoreSchedule:FirestoreSchedule,completionWithPayload:CompletionHandlerWithData?){
+        let userId = Constants.currentLoggedInFireStoreUser?.id
+        
+        if let _userId = userId{
+            let schedulesDocumentRef = Firestore.firestore().collection(FirestoreCollections.users.rawValue).document(_userId)
+            
+            guard let firestoreScheduleAsDictionary = firestoreSchedule.getDictionary() else {
+                completionWithPayload?(false,"Failed to Encode New User Object ",nil)
+                return
+            }
+            schedulesDocumentRef.setData(firestoreScheduleAsDictionary) { err in
+                if let err = err {
+                    completionWithPayload?(false,err.localizedDescription,nil)
+                    print("Error writing document: \(err)")
+                } else {
+                    completionWithPayload?(true,nil,nil)
+                    print("Document successfully written!")
+                }
+            }
+        }else{
+            completionWithPayload?(false,nil,nil)
+        }
+        
+    }
+    
 }
