be8e2a440d0827cda4765c5a63bf3f319bdf031c
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-03 12:32:49 +0530

    Update FirestoreScheduleExercise.swift

--- FitnessApp/FitnessApp/FirestoreDb/FirestoreScheduleExercise.swift
+++ FitnessApp/FitnessApp/FirestoreDb/FirestoreScheduleExercise.swift
@@ -1,10 +1,10 @@
-struct FirestoreScheduleExercise: Codable
+class FirestoreScheduleExercise: Codable
 {
     var excerciseId:String
     var reps: Int
     var sets: Int
     var weight: Int?
-    var totalTime: Int 
+    var totalTime: Int // in  seconds
     var startedAt: String
     var elapsedTime: Int  // milli seconds
 }
