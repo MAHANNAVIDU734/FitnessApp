4952f39802604d27822d4afba82962727536da04
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-03 12:16:52 +0530

    Update Constants.swift

--- FitnessApp/FitnessApp/Helpers/Constants.swift
+++ FitnessApp/FitnessApp/Helpers/Constants.swift
@@ -5,4 +5,5 @@ import FirebaseAuth
 class Constants {
     static  var currentLoggedInFirebaseAuthUser : User?
     static var currentLoggedInFireStoreUser : FirestoreUser?
+    static var exerciseDataOnFirestore : [FirestoreExcercise]?
 }
