f6ec70027475e39f107ee2af415efa0af065f5f6
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-09 22:47:33 +0530

    Update ScheduleVC.swift

--- FitnessApp/FitnessApp/VC/CustomSchedules/Listing/ScheduleVC.swift
+++ FitnessApp/FitnessApp/VC/CustomSchedules/Listing/ScheduleVC.swift
@@ -1,19 +1,24 @@
 
 import UIKit
+import RappleProgressHUD
 
 class ScheduleVC: UIViewController {
     
     @IBOutlet weak var buttonView: UIView!
     @IBOutlet weak var tableView: UITableView!
     
+    private var  firestoreScheduleList = [FirestoreSchedule]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        fetchScheduleListFromFirestore()
     }
     
     func setupView() {
         buttonView.layer.cornerRadius = 30
     }
+    
     func presentPopupView() {
         let vc = ApplicationServiceProvider.shared.viewController(in: .Schedule, identifier: "SchedulePopUpVC")
         if let _vc = vc as? SchedulePopUpVC {
@@ -23,26 +28,50 @@ class ScheduleVC: UIViewController {
         }
         UIApplication.topViewController()?.present(vc, animated: true)
     }
+    
     func navigateToMyExcerciseView() {
         let vc = ApplicationServiceProvider.shared.viewController(in: .Schedule, identifier: "MyExcercieVC")
         self.navigationController?.pushViewController(vc, animated: true)
     }
+    
     @IBAction func addNewScheduleAction(_ sender: Any) {
 //        presentPopupView()
-        let vc = ApplicationServiceProvider.shared.viewController(in: .Schedule, identifier: "PickExcerciseVC")
-        UIApplication.topViewController()?.present(vc, animated: true)
+        navigateToMyExcerciseView()
+    }
+    
+    private func fetchScheduleListFromFirestore() {
+        RappleActivityIndicatorView.startAnimating()
+        FirestoreScheduleManager.shared.getScheduleDataStoredOnFirestoreDb { status, message, data in
+            if (status){
+                print("Schedule List Fetched******")
+                let firestoreScheduleData = data as? [FirestoreSchedule]
+                if  let _firestoreScheduleData = firestoreScheduleData {
+                    self.firestoreScheduleList.removeAll()
+                    self.firestoreScheduleList.insert(contentsOf: _firestoreScheduleData, at: 0)
+                }
+                self.tableView.reloadData()
+                RappleActivityIndicatorView.stopAnimation()
+            }else{
+                RappleActivityIndicatorView.stopAnimation()
+                self.showErrorAlert(messageString: "Something Went Wrong..Please Try Again !")
+            }
+        }
+    }
+    
+    private func showErrorAlert(messageString:String){
+        AlertManager.shared.singleActionMessage(title: "Alert", message: messageString, actionButtonTitle: "Ok", vc: self)
     }
 }
 
 extension ScheduleVC: UITableViewDelegate , UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return firestoreScheduleList.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SchedulTVCell" , for: indexPath)
         if let _cell = cell as? SchedulTVCell {
-            _cell.configCell()
+            _cell.configCell(firestoreSchedule: firestoreScheduleList[indexPath.row])
         }
         return cell
     }
