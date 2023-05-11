77435e158ffe1ccaf56f185321e9c12e40120e27
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-11 21:42:12 +0530

    Update ExerciseDetailVC.swift

--- FitnessApp/FitnessApp/VC/CustomSchedules/Create/AddToSchedule/ExerciseDetailVC.swift
+++ FitnessApp/FitnessApp/VC/CustomSchedules/Create/AddToSchedule/ExerciseDetailVC.swift
@@ -2,15 +2,21 @@
 import UIKit
 
 class ExerciseDetailVC: UIViewController {
-
+    
     @IBOutlet weak var gifImgView: UIImageView!
     @IBOutlet weak var repCountTxt: UITextField!
     @IBOutlet weak var setCountTxt: UITextField!
     @IBOutlet weak var weightLossTxt: UITextField!
     
+    var selectedFirestoreExercise:FirestoreExcercise?=nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bindExecerciseDetailsToUi()
+    }
+    
+    private func bindExecerciseDetailsToUi(){
+        gifImgView.kf.setImage(with: URL(string: selectedFirestoreExercise?.exerciseGIFs.first ?? ""))
     }
     
     @IBAction func addToScheduleAction(_ sender: Any) {
