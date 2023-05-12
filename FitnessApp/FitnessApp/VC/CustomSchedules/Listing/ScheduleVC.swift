e8105abc5f6bdca3a0b69516881897147a207a34
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-12 22:45:01 +0530

    Update ScheduleVC.swift

--- FitnessApp/FitnessApp/VC/CustomSchedules/Listing/ScheduleVC.swift
+++ FitnessApp/FitnessApp/VC/CustomSchedules/Listing/ScheduleVC.swift
@@ -12,7 +12,12 @@ class ScheduleVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
         fetchScheduleListFromFirestore()
+        super.viewWillAppear(animated)
     }
     
     func setupView() {
@@ -22,20 +27,38 @@ class ScheduleVC: UIViewController {
     func presentEnterScheduleTitlePopupView() {
         let vc = ApplicationServiceProvider.shared.viewController(in: .Schedule, identifier: "CreateSchedulePopUpVC")
         if let _vc = vc as? CreateSchedulePopUpVC {
-            _vc.callBack = { status, message in
+            _vc.callBack = { status, scheduleTitle in
                 if status {
-                    self.navigateToSelectExcerciseView()
+                    self.createNewScheduleOnFirestore(scheduleTitle:scheduleTitle! )
                 }else{
                     self.showErrorAlert(messageString: "Schedule Title Required!")
                 }
-                
+                UIApplication.topViewController()?.dismiss(animated: true)
             }
         }
         UIApplication.topViewController()?.present(vc, animated: true)
     }
     
-    func navigateToSelectExcerciseView() {
+    private func createNewScheduleOnFirestore(scheduleTitle:String){
+        RappleActivityIndicatorView.startAnimating()
+        let firestoreSchedule =  FirestoreSchedule(scheduleId: CommonHelpers.randomString(lenth: 12), scheduleTitle: scheduleTitle)
+        FirestoreScheduleManager.shared.createNewScheduleOnFirestoreDb(firestoreSchedule: firestoreSchedule) { status, message, data in
+            if (status){
+                print("Schedule Created******")
+                RappleActivityIndicatorView.stopAnimation()
+                self.navigateToSelectExcerciseView(firestoreSchedule: firestoreSchedule)
+            }else{
+                RappleActivityIndicatorView.stopAnimation()
+                self.showErrorAlert(messageString: "Something Went Wrong..Please Try Again !")
+            }
+        }
+    }
+    
+    func navigateToSelectExcerciseView(firestoreSchedule:FirestoreSchedule) {
         let vc = ApplicationServiceProvider.shared.viewController(in: .Schedule, identifier: "PickExcerciseVC")
+        if let _vc = vc as? PickExcerciseVC {
+            _vc.firestoreSchedule = firestoreSchedule
+        }
         UIApplication.topViewController()?.present(vc, animated: true)
     }
     
