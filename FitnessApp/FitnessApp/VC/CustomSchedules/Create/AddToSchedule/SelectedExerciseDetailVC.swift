131f137d55334a78952002e98c96e12b67279b33
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-13 12:21:03 +0530

    Update SelectedExerciseDetailVC.swift

--- FitnessApp/FitnessApp/VC/CustomSchedules/Create/AddToSchedule/SelectedExerciseDetailVC.swift
+++ FitnessApp/FitnessApp/VC/CustomSchedules/Create/AddToSchedule/SelectedExerciseDetailVC.swift
@@ -1,8 +1,9 @@
 
 import UIKit
+import RappleProgressHUD
 
 class SelectedExerciseDetailVC: UIViewController {
-
+    
     @IBOutlet weak var exerciseTitleLbl: UILabel!
     @IBOutlet weak var gifImgView: UIImageView!
     @IBOutlet weak var repCountTxt: UITextField!
@@ -13,6 +14,7 @@ class SelectedExerciseDetailVC: UIViewController {
     @IBOutlet weak var equipmentLbl: UILabel!
     
     var selectedFirestoreExercise:FirestoreExcercise?=nil
+    var selectedFirestoreSchedule:FirestoreSchedule?=nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,5 +31,88 @@ class SelectedExerciseDetailVC: UIViewController {
     }
     
     @IBAction func addToScheduleAction(_ sender: Any) {
+        if(validateForm()){
+            updateFirestoreScheduleExercise()
+        }
+    }
+    
+    private func updateFirestoreScheduleExercise(){
+        let  firestoreScheduleExercise =   getFirestoreScheduleExerciseObject()
+        RappleActivityIndicatorView.startAnimating()
+        selectedFirestoreSchedule?.exerciseList.append(firestoreScheduleExercise!)
+        FirestoreScheduleManager.shared.updaetCustomScheduleOnFirestoreDb(firestoreSchedule: selectedFirestoreSchedule!) { status, message, data in
+            if (status){
+                print("Schedule Updated******")
+                RappleActivityIndicatorView.stopAnimation()
+                AlertManager.shared.singleActionMessage(title: "Alert", message:"Added Exercise to Schedule! "  , actionButtonTitle: "Ok", vc: self) { action in
+                    UIApplication.topViewController()?.dismiss(animated: true)
+                }
+            }else{
+                RappleActivityIndicatorView.stopAnimation()
+                self.showErrorAlert(messageString: "Something Went Wrong..Please Try Again !")
+            }
+        }
+    }
+    
+    private func getFirestoreScheduleExerciseObject()->FirestoreScheduleExercise?{
+        
+        let repCountInt =  (Int(repCountTxt.text!) ?? 0)
+        let setCountInt =  (Int(setCountTxt.text!) ?? 0)
+        let requiredWeight = Double(weightLossTxt.text ?? "0.0") ?? 0.0
+        
+        let exerciseTimeInMinutes =  (selectedFirestoreExercise?.duration ?? 0)
+        
+        let totalExerciseTimeInMinutes = (( exerciseTimeInMinutes * repCountInt) * setCountInt)
+        let totalExerciseTimeInSeconds = totalExerciseTimeInMinutes * 60
+        
+        let firestoreScheduleExercise = FirestoreScheduleExercise(excerciseId: selectedFirestoreExercise!.excerciseId, reps: repCountInt, sets: setCountInt, weight:requiredWeight,totalTime: totalExerciseTimeInSeconds, elapsedTime: 0)
+        
+        return firestoreScheduleExercise
+        
+    }
+    
+    private func validateForm() -> Bool {
+        
+        let setCount :String? = setCountTxt.text
+        let repCount :String? = repCountTxt.text
+        
+        guard let _repCount = repCount else {
+            showErrorAlert(messageString: "Rep Count Required!")
+            return false
+        }
+        guard !(_repCount.trimLeadingTralingNewlineWhiteSpaces().isEmpty) else {
+            showErrorAlert(messageString: "Enter Valid Rep Count !")
+            return false
+        }
+        
+        let repCountInt =  (Int(_repCount) ?? 0)
+        
+        guard ( repCountInt > 0) else {
+            showErrorAlert(messageString: "Rep Count Has to be Greater than 0")
+            return false
+        }
+        
+        guard let _setCount = setCount else {
+            showErrorAlert(messageString: "Set Count Required!")
+            return false
+        }
+        guard !(_setCount.trimLeadingTralingNewlineWhiteSpaces().isEmpty) else {
+            showErrorAlert(messageString: "Enter Valid Set Count !")
+            return false
+        }
+        
+        let setCountInt =  (Int(_setCount) ?? 0)
+        
+        
+        guard ( setCountInt > 0) else {
+            showErrorAlert(messageString: "Set Count Has to be Greater than 0")
+            return false
+        }
+        
+        return true
+    }
+    
+    private func showErrorAlert(messageString:String){
+        AlertManager.shared.singleActionMessage(title: "Alert", message: messageString, actionButtonTitle: "Ok", vc: self)
     }
 }
