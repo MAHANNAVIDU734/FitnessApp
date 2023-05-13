486d20f96f689a6f3481870d0a0ae64d6e0649ad
MAHANNAVIDU735 <mahanmalporu34@gmail.com>
2023-05-13 12:21:16 +0530

    Update FirestoreSchedule.swift

--- FitnessApp/FitnessApp/FirestoreDb/FirestoreSchedule.swift
+++ FitnessApp/FitnessApp/FirestoreDb/FirestoreSchedule.swift
@@ -5,5 +5,5 @@ struct FirestoreSchedule: Codable
     var elapsedTime: Int = 0  // milli seconds
     var totalTime: Int = 0  // in  seconds
     var status:String = StatesForOngoingActivity.Idle.rawValue
-    var exerciseList : [FirestoreScheduleExercise]? = nil
+    var exerciseList : [FirestoreScheduleExercise] = [FirestoreScheduleExercise]()
 }
